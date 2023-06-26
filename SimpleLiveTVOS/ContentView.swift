0770017db05d41a78eebe4cec4416b25b3c101bc
pc <pangchongcyc@gmail.com>
2023-06-26 11:06:26 +0800

    Initial Commit

---
+++ SimpleLiveTVOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SimpleLiveTVOS
+//
+//  Created by pc on 2023/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleLiveTVOS/SimpleLiveTVOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLiveTVOSApp.swift
+//  SimpleLiveTVOS
+//
+//  Created by pc on 2023/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLiveTVOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
