4d9a950eef967aff92a022966788ebf8ae4b5f8c
pangchong <pc10300@hotmail.com>
2023-07-03 22:52:45 +0800

    左侧列表小优化

--- SimpleLiveTVOS/LeftMenu.swift
+++ SimpleLiveTVOS/LeftMenu.swift
@@ -11,6 +11,7 @@ struct LeftMenu: View {
     
     @Binding var size : CGFloat
     @State var leftMenuIsFocusedArray: Array<Bool> = [false, false, false, false, false]
+    @State var isOpen: Bool = false
     
     var body : some View{
         VStack{
@@ -18,41 +19,49 @@ struct LeftMenu: View {
                 Button(action: {
                     
                 }) {
-                    LeftMenuButton(index: 0 ,doSomething: { index, isFocused in
+                    LeftMenuButton(index: 0, isToggleOn: $isOpen, doSomething: { index, isFocused in
                         self.showOrHide(index: index, isFocused: isFocused)
                     })
+                    
                 }
+                .frame(height: 100)
                 .buttonStyle(CardButtonStyle())
             }
             HStack{
                 Button(action: {
                     
                 }) {
-                    LeftMenuButton(index: 1 ,doSomething: { index, isFocused in
+                    LeftMenuButton(index: 1, isToggleOn: $isOpen, doSomething: { index, isFocused in
                         self.showOrHide(index: index, isFocused: isFocused)
                     })
+
                 }
+                .frame(height: 100)
                 .buttonStyle(CardButtonStyle())
             }
             HStack{
                 Button(action: {
                     
                 }) {
-                    LeftMenuButton(index: 2 ,doSomething: { index, isFocused in
+                    LeftMenuButton(index: 2, isToggleOn: $isOpen, doSomething: { index, isFocused in
                         self.showOrHide(index: index, isFocused: isFocused)
                     })
+                  
                 }
+                .frame(height: 100)
                 .buttonStyle(CardButtonStyle())
             }
             HStack{
                 Button(action: {
                     
                 }) {
-                    LeftMenuButton(index: 3 ,doSomething: { index, isFocused in
+                    LeftMenuButton(index: 3, isToggleOn: $isOpen, doSomething: { index, isFocused in
                         
                         self.showOrHide(index: index, isFocused: isFocused)
                     })
+
                 }
+                .frame(height: 100)
                 .buttonStyle(CardButtonStyle())
             }
             
@@ -60,10 +69,12 @@ struct LeftMenu: View {
                 Button(action: {
                     
                 }) {
-                    LeftMenuButton(index: 4 ,doSomething: { index, isFocused in
+                    LeftMenuButton(index: 4, isToggleOn: $isOpen, doSomething: { index, isFocused in
                         self.showOrHide(index: index, isFocused: isFocused)
                     })
+
                 }
+                .frame(height: 100)
                 .buttonStyle(CardButtonStyle())
             }
             Spacer()
@@ -76,15 +87,18 @@ struct LeftMenu: View {
         withAnimation {
             self.leftMenuIsFocusedArray[index] = isFocused
             var flag = 0
+            print(self.leftMenuIsFocusedArray)
             for itemFocused in self.leftMenuIsFocusedArray {
                 if itemFocused == true {
                     self.size = 300
                     flag = 1
+                    self.isOpen = true
                     break
                 }
             }
             if flag == 0 {
                 self.size = 100
+                self.isOpen = false
             }
         }
     }
@@ -95,18 +109,24 @@ struct LeftMenuButton: View {
     
     @Environment(\.isFocused) private var isFocused : Bool
     var index: Int
+    @Binding var isToggleOn: Bool
     var doSomething: (Int, Bool) -> Void = { _,_  in }
     
     var body: some View {
         HStack {
             Image("bilibili")
-                .frame(width: 30, height: 30)
-            if isFocused {
-                Text(isFocused ? "英雄联盟2": "英雄联盟")
+                .frame(width: 40, height: 40)
+                .padding(.leading, isToggleOn ? 20 : 0)
+            if isToggleOn {
+                Text("英雄联盟")
+//                    .padding(.trailing, 20)
+                    .padding(.leading, -20)
             }
-        }
-        .onChange(of: isFocused) { newValue in
             
+        }
+        .frame(width: isToggleOn ? 260 : 100, height: 100)
+        .onChange(of: isFocused) { newValue in
+            print("index:\(index) isFocused:\(newValue)")
             self.doSomething(index, newValue)
         }
     }
