27a38f2090a9852c0d12d058c13bfb170fb686bb
pangchong <pangchongcyc@gmail.com>
2025-02-21 16:58:09 +0800

    过滤虎牙HDR（无法播放）

--- SimpleLiveTVOS/Source/Search/SearchStore.swift
+++ SimpleLiveTVOS/Source/Search/SearchStore.swift
@@ -15,5 +15,5 @@ class SearchViewModel {
     var searchTypeArray = ["关键词", "链接/分享口令/房间号", "Youtube链接/VideoId"]
     var searchTypeIndex = 0
     var page = 0
-    var searchText: String = "9- #在抖音，记录美好生活#【阳哥ing（丛林肉搏）】正在直播，来和我一起支持Ta吧。复制下方链接，打开【抖音】，直接观看直播！ https://v.douyin.com/iPGmbe4S/ 9@5.com :4pm"
+    var searchText: String = ""
 }
